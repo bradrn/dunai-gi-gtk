5e88d9f1889bbd0e318e86ad8ec5e487f29f5274
Brad Neimann <brad.neimann@hotmail.com>
2023-04-28 22:17:03 +1000

    Improve container implementations

--- app/FRP/Gtk/Containers.hs
+++ app/FRP/Gtk/Containers.hs
@@ -4,7 +4,11 @@
 {-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE OverloadedLabels #-}
 
-module FRP.Gtk.Containers where
+module FRP.Gtk.Containers
+       ( hpaned
+       , Some(..)
+       , box
+       ) where
 
 import Control.Monad.IO.Class (liftIO)
 import Data.Foldable (for_)
@@ -15,6 +19,7 @@ import qualified GI.Gtk as Gtk
 
 import Control.Monad.Trans.MSF
 import Data.MonadicStreamFunction
+import Data.MonadicStreamFunction.InternalCore (MSF(..))
 import FRP.BearRiver
 
 import FRP.Dynamic
@@ -30,25 +35,32 @@ unparent w = Gtk.widgetGetParent w >>= \case
 hpaned :: (Gtk.IsWidget w1, Gtk.IsWidget w2) => SF UI (Event (w1, w2)) (Dynamic Gtk.Paned)
 hpaned = performOnFirstSample $ do
     p <- new Gtk.Paned [ #orientation := Gtk.OrientationHorizontal ]
-    pure $ arrM $ \case
+    pure $ nowDyn $ arrM $ \case
         NoEvent -> pure $ Dynamic False p
         Event (w1, w2) -> liftIO $ do
             unparent w1
             unparent w2
             #add1 p w1
             #add2 p w2
-            pure $ Dynamic True p
+            pure $ Dynamic False p
+
+nowDyn :: Monad m => MSF m a (Dynamic b) -> MSF m a (Dynamic b)
+nowDyn m = MSF $ \a -> do
+    (Dynamic _ b, m') <- unMSF m a
+    pure (Dynamic True b, m')
 
 data Some c = forall w. c w => Some w
 
 box :: Gtk.Orientation -> SF UI (Event [(Some Gtk.IsWidget, Bool, Bool, Word32)]) (Dynamic Gtk.Box)
 box o = performOnFirstSample $ do
     b <- new Gtk.Box [ #orientation := o ]
-    pure $ arrM $ \case
+    pure $ nowDyn $ arrM $ \case
         NoEvent -> pure $ Dynamic False b
         Event ws -> liftIO $ do
+            #foreach b $ \w -> #remove b w
             for_ ws $ \(Some w', expand, fill, pad) -> do
                 w <- Gtk.toWidget w'
                 unparent w
                 #packStart b w expand fill pad
-            pure $ Dynamic True b
+            #showAll b
+            pure $ Dynamic False b
